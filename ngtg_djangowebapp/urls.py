4978dcfa494f3c1643903d27de7702473ac11123
ninogabrielgomez <ngtgomez.student@ua.edu.ph>
2026-09-11 20:26:05 +0800

    Add REST API authentication and session management

--- ngtg_djangowebapp/urls.py
+++ ngtg_djangowebapp/urls.py
@@ -4,5 +4,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
-    path('registration/', include('registration.urls')),  
+    path('registration/', include('registration.urls')),
+    path('api/', include('registration.api_urls')),
+    path('accounts/', include('django.contrib.auth.urls')),  
 ]
